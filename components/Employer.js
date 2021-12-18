50d0be808eaa4f5bfbeec47eec5f0067661331b2
BasakKamil <Bastric91@gmail.com>
2021-12-18 16:31:18 +0100

    new style

--- components/Employer.js
+++ components/Employer.js
@@ -1,7 +1,7 @@
 const Employer = () => {
     return ( 
     <div className="h-100 p-5 bg-light border rounded-3">
-        <h2>2</h2>
+        {/* <h2>2</h2> */}
         <p>
             Zatrudniamy zespół wykwalifikowanych specjalistów, którzy będą mogli udzielić wsparcia pracodawcom, odpowiadając na ich aktualne potrzeby kadrowe. Jesteśmy elastyczni wobec potrzeb naszych Klientów i każdą ofertę dostosujemy do potencjalnych Klientów.
             Nasi doradcy współpracują z Akademickimi Biurami Karier, Urzędami Pracy, biorą udział w licznych wydarzeniach branży HR i tym samym czynnie uczestniczą w budowaniu polskiego rynku pracy. Pomagamy nie tylko aktywować osoby bezrobotne i prowadzić spotkania ze studentami, którzy dopiero rozpoczynają życie zawodowe, lecz także szukamy nowych rozwiązań dla osób chcących zmienić swój profil zawodowy.
